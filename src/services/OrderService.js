fa92e7b43da262752c16eddea3439447561493ec
Jason <jmaalegado@gmail.com>
2022-03-21 22:49:47 +1100

    service api file added

---
+++ src/services/OrderService.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:3000',
+  withCredentials: false,
+});
+
+export default {
+  getOrders() {
+    // console.log('hello world, hey there');
+    return apiClient.get('/orders');
+  },
+};
